5a4a18f6ff7e248cb8ea230aa004bc2a47864257
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-05-01 23:26:27 +0800

    Added fake users to be seeded

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10), // hashSync() might not be the ideal idea, but in this case I've just got 3 users data to seed, so I'll forgive myself for it. ;)
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
