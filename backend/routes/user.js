e5c2f763ae193b5a38f1ee9c3bc9800a46410823
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-05-06 21:54:49 +0800

    Almost done with jwt.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { authenticateUser } from "../controllers/user.js";
+import { authenticateUser, getUserProfile } from "../controllers/user.js";
+import { protectRoute } from "../middleware/auth.js";
 
 router.post("/login", authenticateUser);
+router.route("/profile").get(protectRoute, getUserProfile);
 
 export default router;
