0a2ef2dfb98aed5a9ae7094f1e34cdf1db5e3b2e
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-05-31 21:44:08 +0800

    Added getUserDetails action.

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -88,47 +88,39 @@ export const register = (name, email, password) => async (dispatch) => {
   }
 };
 
-// export const getUserDetails = () => async (dispatch) => {
-//   try {
-//     dispatch({
-//       type: USER_REGISTER_REQUEST,
-//     });
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
 
-//     const config = {
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     };
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-//     const { data } = await axios.post(
-//       "/api/users",
-//       { name, email, password },
-//       config
-//     );
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-//     dispatch({
-//       type: USER_REGISTER_SUCCESS,
-//       payload: data,
-//     });
+    const { data } = await axios.post(`/api/users/${id}`, config);
 
-//     // In this implementation, users will be logged in right away upon successful registration.
-
-//     dispatch({
-//       type: USER_LOGIN_SUCCESS,
-//       payload: data,
-//     });
-
-//     localStorage.setItem("userInfo", JSON.stringify(data));
-//   } catch (error) {
-//     dispatch({
-//       type: USER_REGISTER_FAIL,
-//       payload:
-//         error.response && error.response.data.message
-//           ? error.response.data.message
-//           : error.message,
-//     });
-//   }
-// };
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
