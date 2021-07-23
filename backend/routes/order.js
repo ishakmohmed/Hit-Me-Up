df7cfd1637af90aad2d12b5a8dfade9b68406198
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-07-23 22:48:05 +0800

    Added NextJS template.

--- backend/routes/order.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/order.js";
-import { protectRoute } from "../middleware/auth.js";
-
-router.route("/").post(protectRoute, addOrderItems);
-router.route("/:id").get(protectRoute, getOrderById);
-
-export default router;
--- backend/routes/product.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { getProducts, getProductById } from "../controllers/product.js";
-
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
-
-export default router;
