519d5fc03df4f213e6a53e1e9c67e3b876139d27
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-07-25 02:42:07 +0800

    Last commit of the day.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const next = require("next");
+require("dotenv").config({ path: "./.env.local" });
+
+const connectDb = require("./utilsServer/connectDb");
+
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
-
-require("dotenv").config({ path: "./.env.local" });
-const connectDb = require("./utilsServer/connectDb");
 
 connectDb();
 
 app.use(express.json());
-
-const dev = process.env.NODE_ENV !== "production";
 
 const PORT = process.env.PORT || 3000;
 
