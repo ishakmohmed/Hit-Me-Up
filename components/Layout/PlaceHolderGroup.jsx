e2da16397213984476475bc56a182e6371900b94
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-08-19 15:19:48 +0800

    Last commit of the day.

--- components/Layout/PlaceHolderGroup.jsx
+++ components/Layout/PlaceHolderGroup.jsx
@@ -1,16 +1,8 @@
-import {
-  Placeholder,
-  Divider,
-  List,
-  Button,
-  Card,
-  Container,
-  Icon
-} from "semantic-ui-react";
+import { Placeholder, Divider, Container, Icon } from "semantic-ui-react";
 import { range } from "lodash";
 
 export const PlaceHolderPosts = () =>
-  range(1, 3).map(item => (
+  range(1, 3).map((item) => (
     <div key={item}>
       <Placeholder fluid>
         <Placeholder.Header image>
@@ -28,49 +20,6 @@ export const PlaceHolderPosts = () =>
     </div>
   ));
 
-export const PlaceHolderSuggestions = () => (
-  <>
-    <List.Item>
-      <Card color="red">
-        <Placeholder>
-          <Placeholder.Image square />
-        </Placeholder>
-        <Card.Content>
-          <Placeholder>
-            <Placeholder.Header>
-              <Placeholder.Line length="medium" />
-            </Placeholder.Header>
-          </Placeholder>
-        </Card.Content>
-
-        <Card.Content extra>
-          <Button
-            disabled
-            circular
-            size="small"
-            icon="add user"
-            content="Follow"
-            color="twitter"
-          />
-        </Card.Content>
-      </Card>
-    </List.Item>
-  </>
-);
-
-export const PlaceHolderNotifications = () =>
-  range(1, 10).map(item => (
-    <>
-      <Placeholder key={item}>
-        <Placeholder.Header image>
-          <Placeholder.Line />
-          <Placeholder.Line />
-        </Placeholder.Header>
-      </Placeholder>
-      <Divider hidden />
-    </>
-  ));
-
 export const EndMessage = () => (
   <Container textAlign="center">
     <Icon name="refresh" size="large" />
@@ -79,7 +28,7 @@ export const EndMessage = () => (
 );
 
 export const LikesPlaceHolder = () =>
-  range(1, 6).map(item => (
+  range(1, 6).map((item) => (
     <Placeholder key={item} style={{ minWidth: "200px" }}>
       <Placeholder.Header image>
         <Placeholder.Line length="full" />
