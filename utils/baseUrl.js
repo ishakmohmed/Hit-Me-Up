7bfca2558fdf2b807d3996889c99f430a57e199b
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-10-02 01:22:03 +0800

    Make it better.

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : "https://hit-me-up-mern-app.herokuapp.com/";
 
 module.exports = baseUrl;
